4046863e45f16792c3287b4b6c3a4fe3f6556089
yum <contact@memyu.com>
2025-11-29 23:39:07 -0500

    init: set up cargo and nix flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Kill Ports";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+    pkgsForEach = nixpkgs.legacyPackages;
+  in {
+    devShells = forEachSystem (system: {
+      default = pkgsForEach.${system}.callPackage ./nix/shell.nix {};
+    });
+  };
+}
